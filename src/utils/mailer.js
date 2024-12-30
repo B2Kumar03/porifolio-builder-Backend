79f30919dd65b9ac02671d270ccbdd2d67d5a7e9
Bittu Kumar <bk7355583@gmail.com>
2024-12-31 00:51:07 +0530

    Added.

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,29 +1,39 @@
 // mailer.js
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: 'Gmail', // or any other email service
+  service: "Gmail", // or any other email service
   auth: {
     user: process.env.EMAIL_USER, // your email
     pass: process.env.EMAIL_PASS, // your email password
   },
 });
 
-const sendOTP = (email,name, otp) => {
+const sendOTP = (email, name, otp) => {
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: email,
-    subject: 'Thank you for registering'+" "+name,
+    subject: "Thank you for registering" + " " + name,
     text: `
     We are excited to have you on board. Your registration is complete, and you can now explore all the features we offer. If you have any questions or need further assistance, feel free to reach out to our support team.
     `,
     html: `
     <h1>Thank you for registering</h1>
     <p>Dear ${name},
-    We are excited to have you on board. Your registration is complete, and you can now explore all the features we offer. If you have any questions or need further assistance, feel free to reach out to our support team.
+    We are excited to have you on board. Your registration is complete, and you can now explore all the features we offer. If you have any questions or need further assistance, feel free to reach out to me.
 
     </p>
-    <a>https://bittu-kumar.netlify.app/ </a>`
+    
+    <a href="https://bittu-kumar.netlify.app/">
+    <button 
+    style="
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;"
+    >Click here to connect</button>
+    `,
   };
 
   return transporter.sendMail(mailOptions);
