1284cbee31b5dc1638254691ccfe2ddbb578cd15
Bittu Kumar <bk7355583@gmail.com>
2024-12-30 23:04:37 +0530

    Added.

--- src/controllers/ai.controllers.js
+++ src/controllers/ai.controllers.js
@@ -5,7 +5,6 @@ global.Headers = Headers;
 
 const genAi = new GoogleGenerativeAI(process.env.API_KEY);
 export const generator = async (prompt) => {
-  
   try {
     const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
     let text;
@@ -15,7 +14,7 @@ export const generator = async (prompt) => {
     return text;
   } catch (error) {
     console.error(error);
-    return null
+    return null;
   }
 };
 
@@ -28,14 +27,11 @@ export const generateDetails = async (req, res) => {
     let result = await model.generateContent(pt);
     let response = await result.response;
     text = response.text();
-    text = `I am ${name}` + " " + text;
     res.status(200).json({ message: text, success: true });
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({
-        error: `An error occurred while generating the content. ${error}`,
-      });
+    res.status(500).json({
+      error: `An error occurred while generating the content. ${error}`,
+    });
   }
 };
