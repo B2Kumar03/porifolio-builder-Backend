4c81e1f7182a2bab8c3b0f9d45c97a37e345cd5f
Bittu Kumar <bk7355583@gmail.com>
2024-12-31 00:28:40 +0530

    Added.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,13 +6,12 @@ import {
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 import accessAuthMiddleware from "../middlewares/accessAuth.middleware.js";
-import { userData } from "../controllers/userData.controller.js";
+
 
 const userRouter = Router();
 
 userRouter.route("/register").post(upload.single("avtar"), registerUser);
 userRouter.route("/login").post(login);
 userRouter.route("/getuser").get(accessAuthMiddleware, getUser);
-userRouter.route("/saveDetails", userData);
 
 export default userRouter;
